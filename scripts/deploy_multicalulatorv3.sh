e85d960d69be66c70b4129c3fd19dc92e9f62abb
Dennis Zielke <dennis.zielke@microsoft.com>
2020-03-02 16:12:59 +0100

    added release script

---
+++ scripts/deploy_multicalulatorv3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Starting release"
+echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
+echo "AGENT_WORKFOLDER contents:"
+ls -1 $AGENT_WORKFOLDER
+echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
+echo "Build Id is $BUILD_BUILDNUMBER and $BUILD_ID"
+echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY"
+AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY.azurecr.io
+echo "Azure Container Registry Url is $AZURE_CONTAINER_REGISTRY_URL"
