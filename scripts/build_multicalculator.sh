c580ced00bc51ba6163e6bc7c4b22cd1220008cb
Dennis Zielke <dennis.zielke@microsoft.com>
2020-03-02 16:57:08 +0100

    renamed variable

--- scripts/build_multicalculator.sh
+++ scripts/build_multicalculator.sh
@@ -9,8 +9,8 @@ ls -1 $AGENT_BUILDDIRECTORY
 echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
 echo "Build Id is $BUILD_BUILDNUMBER and $BUILD_BUILDID"
 echo "Build Sources Folder is $BUILD_SOURCESDIRECTORY"
-echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY"
-AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY.azurecr.io
+echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY_NAME"
+AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY_NAME.azurecr.io
 echo "Azure Container Registry Url is $AZURE_CONTAINER_REGISTRY_URL"
 cd $BUILD_SOURCESDIRECTORY
 ls -l
@@ -28,8 +28,8 @@ docker build -t $AZURE_CONTAINER_REGISTRY_URL/js-calc-frontend:$BUILD_BUILDNUMBE
 echo "Completed building js-calc-frontend container"
 
 echo "Pushing images to $AZURE_CONTAINER_REGISTRY_URL"
-az acr login --name $AZURE_CONTAINER_REGISTRY
-az configure --defaults acr=$AZURE_CONTAINER_REGISTRY
+az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
+az configure --defaults acr=$AZURE_CONTAINER_REGISTRY_NAME
 docker push $AZURE_CONTAINER_REGISTRY_URL/go-calc-backend:$BUILD_BUILDNUMBER
 echo "Completed pusing go-calc-backend container"
 docker push $AZURE_CONTAINER_REGISTRY_URL/js-calc-backend:$BUILD_BUILDNUMBER
@@ -40,7 +40,7 @@ echo "Completed pusing js-calc-frontend container"
 echo "Startig packaging helm chart"
 cd $BUILD_SOURCESDIRECTORY/charts/
 helm package multicalculator --app-version 3.0.$BUILD_BUILDID --version 0.1.$BUILD_BUILDID
-echo "Pushing helm chart to $AZURE_CONTAINER_REGISTRY"
+echo "Pushing helm chart to $AZURE_CONTAINER_REGISTRY_NAME"
 az acr helm repo add
 az acr helm push multicalculator-*.tgz --force
 echo "Pushed helm chart"
--- scripts/deploy_multicalulator.sh
+++ scripts/deploy_multicalulator.sh
@@ -5,12 +5,12 @@ echo "AGENT_WORKFOLDER contents:"
 ls -1 $AGENT_WORKFOLDER
 echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
 echo "Build Id is $BUILD_BUILDNUMBER and $BUILD_ID"
-echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY"
+echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY_NAME"
 AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY.azurecr.io
 echo "Azure Container Registry Url is $AZURE_CONTAINER_REGISTRY_URL"
 
-az acr login --name $AZURE_CONTAINER_REGISTRY
-az configure --defaults acr=$AZURE_CONTAINER_REGISTRY
+az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
+az configure --defaults acr=$AZURE_CONTAINER_REGISTRY_NAME
 az acr helm list --name $ACR_NAME
 
 helm search repo -l $ACR_NAME/multicalculator
