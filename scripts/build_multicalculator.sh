9765660b7c695fc25750f21cd8c46f53a6153205
Dennis Zielke <dennis.zielke@microsoft.com>
2020-03-03 09:25:08 +0100

    simplified

--- scripts/build_multicalculator.sh
+++ scripts/build_multicalculator.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+
+build_push_container () {
+    echo "Starting to build container $1 ..."
+    cd $BUILD_SOURCESDIRECTORY/apps/$1
+    docker build -t $AZURE_CONTAINER_REGISTRY_URL/$1:$BUILD_BUILDNUMBER .
+    echo "Completed building $1 container"
+    docker push $AZURE_CONTAINER_REGISTRY_URL/$1:$BUILD_BUILDNUMBER
+    echo "Completed pusing $1 container"
+}
+
+package_push_helmchart () {
+    echo "Startig packaging helm chart $1 ..."
+    cd $BUILD_SOURCESDIRECTORY/charts/
+    helm package $1 --app-version 3.0.$BUILD_BUILDID --version 0.1.$BUILD_BUILDID
+    echo "Pushing helm chart to $AZURE_CONTAINER_REGISTRY_NAME"
+    az acr helm repo add
+    az acr helm push $1-*.tgz --force
+    echo "Pushed helm chart $1"
+}
+
 echo "Starting build"
 echo "Argument is $1"
 echo "AGENT_WORKFOLDER is $AGENT_WORKFOLDER"
@@ -11,38 +31,15 @@ echo "SYSTEM_HOSTTYPE is $SYSTEM_HOSTTYPE"
 echo "Build Id is $BUILD_BUILDNUMBER and $BUILD_BUILDID"
 echo "Build Sources Folder is $BUILD_SOURCESDIRECTORY"
 echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY_NAME"
-echo "Secret Azure Container Registry value is $phoenixacrname"
 AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY_NAME.azurecr.io
 echo "Azure Container Registry Url is $AZURE_CONTAINER_REGISTRY_URL"
 cd $BUILD_SOURCESDIRECTORY
 ls -l
-echo "Starting to build go-calc-backend container"
-cd $BUILD_SOURCESDIRECTORY/apps/go-calc-backend
-docker build -t $AZURE_CONTAINER_REGISTRY_URL/go-calc-backend:$BUILD_BUILDNUMBER .
-echo "Completed building go-calc-backend container"
-echo "Starting to build js-calc-backend container"
-cd $BUILD_SOURCESDIRECTORY/apps/js-calc-backend
-docker build -t $AZURE_CONTAINER_REGISTRY_URL/js-calc-backend:$BUILD_BUILDNUMBER .
-echo "Completed building js-calc-backend container"
-echo "Starting to build js-calc-frontend container"
-cd $BUILD_SOURCESDIRECTORY/apps/js-calc-frontend
-docker build -t $AZURE_CONTAINER_REGISTRY_URL/js-calc-frontend:$BUILD_BUILDNUMBER .
-echo "Completed building js-calc-frontend container"
-
 echo "Pushing images to $AZURE_CONTAINER_REGISTRY_URL"
 az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
 az configure --defaults acr=$AZURE_CONTAINER_REGISTRY_NAME
-docker push $AZURE_CONTAINER_REGISTRY_URL/go-calc-backend:$BUILD_BUILDNUMBER
-echo "Completed pusing go-calc-backend container"
-docker push $AZURE_CONTAINER_REGISTRY_URL/js-calc-backend:$BUILD_BUILDNUMBER
-echo "Completed pusing js-calc-backend container"
-docker push $AZURE_CONTAINER_REGISTRY_URL/js-calc-frontend:$BUILD_BUILDNUMBER
-echo "Completed pusing js-calc-frontend container"
+build_push_container "go-calc-backend"
+build_push_container "js-calc-backend"
+build_push_container "js-calc-frontend"
 
-echo "Startig packaging helm chart"
-cd $BUILD_SOURCESDIRECTORY/charts/
-helm package multicalculator --app-version 3.0.$BUILD_BUILDID --version 0.1.$BUILD_BUILDID
-echo "Pushing helm chart to $AZURE_CONTAINER_REGISTRY_NAME"
-az acr helm repo add
-az acr helm push multicalculator-*.tgz --force
-echo "Pushed helm chart"
+package_push_helmchart "multicalculator"
--- scripts/deploy_multicalulator.sh
+++ scripts/deploy_multicalulator.sh
@@ -8,9 +8,19 @@ echo "Build Id is $BUILD_BUILDNUMBER and $BUILD_ID"
 echo "Azure Container Registry is $AZURE_CONTAINER_REGISTRY_NAME"
 AZURE_CONTAINER_REGISTRY_URL=$AZURE_CONTAINER_REGISTRY.azurecr.io
 echo "Azure Container Registry Url is $AZURE_CONTAINER_REGISTRY_URL"
+echo "Azure KeyVault is $AZURE_KEYVAULT_NAME"
+
+
+REDIS_HOST=$(az keyvault secret show --name "phoenix-redis-host" --vault-name $AZURE_KEYVAULT_NAME)
+REDIS_AUTH=$(az keyvault secret show --name "phoenix-redis-access" --vault-name $AZURE_KEYVAULT_NAME)
+APPINSIGHTS_KEY=$(az keyvault secret show --name "phoenix-appinsights-key" --vault-name $AZURE_KEYVAULT_NAME)
+
+echo "Redis Host $REDIS_HOST"
+echo "Redis Key $REDIS_AUTH"
+echo "Appinsights $APPINSIGHTS_KEY"
 
 az acr login --name $AZURE_CONTAINER_REGISTRY_NAME
 az configure --defaults acr=$AZURE_CONTAINER_REGISTRY_NAME
-az acr helm list --name $ACR_NAME
+az acr helm list --name $AZURE_CONTAINER_REGISTRY_NAME
 
-helm search repo -l $ACR_NAME/multicalculator
+helm search repo -l $AZURE_CONTAINER_REGISTRY_NAME/multicalculator
