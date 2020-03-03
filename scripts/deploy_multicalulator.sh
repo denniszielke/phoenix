100c6611ddd65a7588268e9cc74a676d00730258
Dennis Zielke <dennis.zielke@microsoft.com>
2020-03-03 18:11:55 +0100

    use proper values

--- scripts/deploy_multicalulator.sh
+++ scripts/deploy_multicalulator.sh
@@ -37,5 +37,5 @@ kubectl get namespace
 kubectl create namespace $KUBERNETES_NAMESPACE
 
 echo "Deploying helm release"
-helm upgrade calculator $AZURE_CONTAINER_REGISTRY_NAME/multicalculator --namespace $KUBERNETES_NAMESPACE --install --set replicaCount=4 --set frontendTag=$BUILD_BUILDNUMBER --set backendTag=$BUILD_BUILDNUMBER --set dependencies.useAppInsights=true --set dependencies.appInsightsSecretValue=$APPINSIGHTS_KEY --set ingress.host=$INGRESS_FQDN --wait --timeout 30s
+helm upgrade calculator $AZURE_CONTAINER_REGISTRY_NAME/multicalculator --namespace $KUBERNETES_NAMESPACE --install --set replicaCount=4 --set image.frontendTag=$BUILD_BUILDNUMBER --set image.backendTag=$BUILD_BUILDNUMBER --set image.repository=$AZURE_CONTAINER_REGISTRY_URL --set dependencies.useAppInsights=true --set dependencies.appInsightsSecretValue=$APPINSIGHTS_KEY --set ingress.host=$INGRESS_FQDN --wait --timeout 30s
 
