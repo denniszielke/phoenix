1c5d41fb44bdb65bcd440a7d9c4e53cd55e3f70c
denniszielke <11569044+denniszielke@users.noreply.github.com>
2021-03-23 18:43:23 +0100

    fix

--- terraform/deploy_tf.sh
+++ terraform/deploy_tf.sh
@@ -9,7 +9,7 @@ echo "No team_name provided - aborting"
 exit 0;
 fi
 
-if [ "$DEPLOYMENT_NAME" =~ ^[a-z0-9]{3,6}$ ]; then
+if [[ $DEPLOYMENT_NAME =~ ^[a-z0-9]{3,6}$ ]]; then
     echo "Deployment $DEPLOYMENT_NAME name is valid"
 else
     echo "Deployment $DEPLOYMENT_NAME name is invalid - only numbers and lower case min 3 and max 6 characters allowed - aborting"
